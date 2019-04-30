29d67586862ced22b5e0f553bdb5bfc0c6f9161b
Jared Palmer <jared@palmer.net>
2019-04-30 10:57:37 -0400

    Upgrade to jest 24 + ts-jest

--- src/createJestConfig.js
+++ src/createJestConfig.js
@@ -1,14 +1,20 @@
 import { appPackageJson } from './constants';
 
 export function createJestConfig(resolve, rootDir) {
+  const compilerOptions = resolve('./tsconfig.json');
   return {
     transform: {
-      '.(ts|tsx)': resolve('../ts-jest'),
+      '.(ts|tsx)': require.resolve('ts-jest/dist'),
     },
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     collectCoverageFrom: ['src/**/*.{ts,tsx}'],
     testMatch: ['<rootDir>/test/**/?(*.)(spec|test).{ts,tsx}'],
+    testURL: 'http://localhost',
     rootDir,
+    watchPlugins: [
+      require.resolve('jest-watch-typeahead/filename'),
+      require.resolve('jest-watch-typeahead/testname'),
+    ],
   };
 }
