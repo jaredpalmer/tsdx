2a5e5a5595ba85c68af9f9fc12370f90ec224d5c
Anton Gilgur <agilgur5@gmail.com>
2019-12-19 10:10:49 -0500

    (types/fix): explicit Rollup typing, fix treeshake location (#371)
    - treeshake was in output, but it's not a config of output :/ :/ :/
      - once explicit types were added, this was a big red underline :/ :/

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ try {
 
 // check for custom tsdx.config.js
 let tsdxConfig = {
-  rollup(config: any, _options: any) {
+  rollup(config: RollupOptions, _options: TsdxOptions): RollupOptions {
     return config;
   },
 };
