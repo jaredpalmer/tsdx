d6def208d937f905388d37d7403cb3ba61e4858b
Jirat Ki <saakyz@gmail.com>
2020-03-01 16:12:17 +0800

    Correct comment link

--- test/fixtures/build-default/src/syntax/jsx-import/JSX-B.jsx
+++ test/fixtures/build-default/src/syntax/jsx-import/JSX-B.jsx
@@ -1,6 +1,6 @@
 // DO NOT IMPORT THIS FILE DIRECTLY FROM index.ts
 // THIS FILE IS INTENTIONALLY TO TEST JSX CHAINING IMPORT
-// SEE https://github.com/jaredpalmer/tsdx/issues/523#issuecomment-592662499
+// SEE https://github.com/jaredpalmer/tsdx/issues/523
 
 export default function JSXComponent() {
   return 'JSXC';
