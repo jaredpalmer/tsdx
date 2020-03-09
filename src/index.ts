edd8045cca3ea0cd156e5b0c6e0d517a04e7a18f
Anton Gilgur <agilgur5@gmail.com>
2020-03-09 16:38:57 -0400

    (deps/fix/types): upgrade rollup & fix event type issues (#544)
    - if TSDX were installed directly from git, the `prepare` script would
      for some reason fail on these events
      - but they wouldn't error when run internally
        - when Rollup was upgraded, then they started erroring
    
    - also remove the unnecessary await in front of watch

--- src/index.ts
+++ src/index.ts
@@ -321,7 +321,7 @@ prog
     }
 
     const spinner = ora().start();
-    await watch(
+    watch(
       (buildConfigs as RollupWatchOptions[]).map(inputOptions => ({
         watch: {
           silent: true,
@@ -341,11 +341,6 @@ prog
         spinner.start(chalk.bold.cyan('Compiling modules...'));
       }
       if (event.code === 'ERROR') {
-        spinner.fail(chalk.bold.red('Failed to compile'));
-        logError(event.error);
-        failureKiller = run(opts.onFailure);
-      }
-      if (event.code === 'FATAL') {
         spinner.fail(chalk.bold.red('Failed to compile'));
         logError(event.error);
         failureKiller = run(opts.onFailure);
