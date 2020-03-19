76177265e6b5f272d0731b521411eec75912eccd
Anton Gilgur <agilgur5@gmail.com>
2020-03-18 22:00:26 -0400

    (test): ensure --extractErrors kind of works
    - the test output strips away `invariant` entirely in all builds,
      instead of replacing it with ErrorProd/ErrorDev etc...
      - it leaves an empty import however...
        - which gives an unused import warning during builds
      - and it doesn't do anything with `warning` at all, it doesn't get
        stripped or changed, and it's message is not in codes.json
        - the source code indeed only checks for 'invariant'
      - and it does generate errors/codes.json, ErrorProd.js, ErrorDev.js
      - I'm not 100% sure, but that seems to be buggy to me
    
    (refactor): split --extractError code into a build-options fixture
    - as it doesn't require tsdx.config.js at all
    - but it is an integration test, as it requires tiny-invariant etc
    
    (clean): remove the errors/ directory as that's auto-generated by
    --extractErrors and is the thing being tested
    
    (clean): remove src/foo.ts as it's extraneous

---
+++ test/integration-tests/fixtures/build-options/src/index.ts
@@ -0,0 +1,12 @@
+import invariant from 'tiny-invariant';
+import warning from 'tiny-warning';
+
+invariant(true, 'error occurred! o no');
+warning(true, 'warning - water is wet');
+
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('fuck');
+  }
+  return a + b;
+};
