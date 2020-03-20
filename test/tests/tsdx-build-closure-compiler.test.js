8ccba4d5eede4771fdcb27d199bb0bea76c8fe0d
ambroseus <ambroseus@gmail.com>
2020-03-20 16:44:29 +0200

    improve tests

--- test/tests/tsdx-build-closure-compiler.test.js
+++ test/tests/tsdx-build-closure-compiler.test.js
@@ -15,6 +15,10 @@ const grep = (pattern, file) => {
 
 const stageName = 'stage-build-closure-compiler';
 
+const cjsBundleDev = 'dist/build-withconfig.cjs.development.js';
+const cjsBundleProd = 'dist/build-withconfig.cjs.production.min.js';
+const esmBundleProd = 'dist/build-withconfig.esm.production.min.js';
+
 describe('tsdx build with closure compiler default options', () => {
   beforeAll(() => {
     util.teardownStage(stageName);
@@ -29,27 +33,16 @@ describe('tsdx build with closure compiler default options', () => {
     expect(output.code).toBe(0);
 
     expect(shell.test('-f', 'dist/index.js')).toBeTruthy();
-    expect(
-      shell.test('-f', 'dist/build-withconfig.cjs.development.js')
-    ).toBeTruthy();
-    expect(
-      shell.test('-f', 'dist/build-withconfig.cjs.production.min.js')
-    ).toBeTruthy();
-    expect(
-      shell.test('-f', 'dist/build-withconfig.esm.production.min.js')
-    ).toBeTruthy();
-
+    expect(shell.test('-f', cjsBundleDev)).toBeTruthy();
+    expect(shell.test('-f', cjsBundleProd)).toBeTruthy();
+    expect(shell.test('-f', esmBundleProd)).toBeTruthy();
     expect(shell.test('-f', 'dist/index.d.ts')).toBeTruthy();
 
     const lib = require(`../../${stageName}/dist`);
     expect(lib.signature).toBe('bar 0');
 
-    expect(
-      grep(
-        'exports.signature=signature',
-        'dist/build-withconfig.cjs.production.min.js'
-      )
-    ).toBeTruthy();
+    expect(grep('exports.signature=signature', cjsBundleProd)).toBeTruthy();
+    expect(grep('bar 0', cjsBundleProd)).toBeFalsy();
   });
 
   it('should minify bundle with advanced options', () => {
@@ -62,16 +55,9 @@ describe('tsdx build with closure compiler default options', () => {
     expect(output.code).toBe(0);
 
     expect(shell.test('-f', 'dist/index.js')).toBeTruthy();
-    expect(
-      shell.test('-f', 'dist/build-withconfig.cjs.development.js')
-    ).toBeTruthy();
-    expect(
-      shell.test('-f', 'dist/build-withconfig.cjs.production.min.js')
-    ).toBeTruthy();
-    expect(
-      shell.test('-f', 'dist/build-withconfig.esm.production.min.js')
-    ).toBeTruthy();
-
+    expect(shell.test('-f', cjsBundleDev)).toBeTruthy();
+    expect(shell.test('-f', cjsBundleProd)).toBeTruthy();
+    expect(shell.test('-f', esmBundleProd)).toBeTruthy();
     expect(shell.test('-f', 'dist/index.d.ts')).toBeTruthy();
 
     const lib = require(`../../${stageName}/dist`);
@@ -79,9 +65,8 @@ describe('tsdx build with closure compiler default options', () => {
 
     // closure compiler with advanced optimization level
     // minifies bundle with `signature="bar 0"`
-    expect(
-      grep('bar 0', 'dist/build-withconfig.cjs.production.min.js')
-    ).toBeTruthy();
+    expect(grep('exports.signature=signature', cjsBundleProd)).toBeFalsy();
+    expect(grep('bar 0', cjsBundleProd)).toBeTruthy();
   });
 
   afterEach(() => {
