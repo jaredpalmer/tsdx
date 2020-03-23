6e5ecee8b841406cdcb3b5be4ef3ec4df51ee7e6
Anton Gilgur <agilgur5@gmail.com>
2020-03-23 00:33:00 -0400

    (clean): remove some unnecessary internal declarations
    - these either aren't directly used in the code or are outdated and no
      longer used in general

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,7 +4,6 @@ declare module 'jpjs';
 declare module 'ora';
 declare module 'tiny-glob/sync';
 declare module 'ansi-escapes';
-declare module 'eslint-config-react-app';
 
 // Patch Babel
 // @see line 226 of https://unpkg.com/@babel/core@7.4.4/lib/index.js
@@ -14,7 +13,6 @@ declare module '@babel/core' {
 }
 
 // Rollup plugins
-declare module '@jaredpalmer/rollup-plugin-preserve-shebang';
 declare module 'rollup-plugin-babel';
 declare module 'rollup-plugin-size-snapshot';
 declare module 'rollup-plugin-terser';
