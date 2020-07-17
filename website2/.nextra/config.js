6dd85e185864b3b8567f361c391fb3f76d8292fe
Jared Palmer <jared@palmer.net>
2020-07-17 11:19:21 -0400

    New website

---
+++ website2/.nextra/config.js
@@ -0,0 +1,11 @@
+import userConfig from '../nextra.config';
+
+const defaultConfig = {
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+};
+
+export default () => {
+  return { ...defaultConfig, ...userConfig };
+};
