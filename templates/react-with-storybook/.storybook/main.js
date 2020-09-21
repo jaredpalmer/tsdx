5c2b0ed44ba5e000316b324a58d54eff94ae2889
Kyle Holmberg <inbox@kylemh.com>
2020-09-21 11:35:11 -0700

    Update templates/react-with-storybook/.storybook/main.js
    Co-authored-by: Anton Gilgur <agilgur5@gmail.com>

--- templates/react-with-storybook/.storybook/main.js
+++ templates/react-with-storybook/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // set to false if you don't care about your stories being type-checked.
   }
