2caad243705219f7aba35cf1d2d4442827da5ff5
Anton Gilgur <agilgur5@gmail.com>
2020-09-20 23:18:39 -0400

    license: add FB License header to all extractErrors code
    - this plugin is largely borrowed from the React monorepo, consistently
      add FB's License headers to each file, not just to extractErrors.ts
    
    - also add comments pointing to the exact file and commit that each
      file was copied from
      - most of these are slightly out of date now:
        - evalToString is missing a line
        - extractErrors needs to be updated to Babel 7
          - babylon -> @babel/parser, babel-traverse -> @babel/traverse
        - transformErrorMessages has several new constructs added :/...
          - keeping a pseudo-fork up-to-date is pretty tedious and not very
            maintainble (it's also broken per the tests I've added)
            - would be better if we could ask FB to split out theirs as a
              separate package...

--- src/errors/transformErrorMessages.ts
+++ src/errors/transformErrorMessages.ts
@@ -1,3 +1,11 @@
+// largely borrowed from https://github.com/facebook/react/blob/2c8832075b05009bd261df02171bf9888ac76350/scripts/error-codes/transform-error-messages.js
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import fs from 'fs';
 import { invertObject } from './invertObject';
 import { evalToString } from './evalToString';
