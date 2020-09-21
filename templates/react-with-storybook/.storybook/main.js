93d7838b444b627942d15822197a7d2d61e846a8
Kyle Holmberg <inbox@kylemh.com>
2020-09-20 18:13:31 -0700

    ensure stories are type-checked

--- templates/react-with-storybook/.storybook/main.js
+++ templates/react-with-storybook/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  typescript: {
+    check: true, // set to false if you don't care about your stories being type-checked.
+  }
 };
