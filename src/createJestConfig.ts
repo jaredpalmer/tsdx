0b7c7a9f597f69e0f94d6883fba40e56d1427e0c
Andrew Bradley <cspotcode@gmail.com>
2020-12-08 14:47:50 -0500

    declare babel-jest dependency matching version of jest

--- src/createJestConfig.ts
+++ src/createJestConfig.ts
@@ -1,6 +1,4 @@
 import { Config } from '@jest/types';
-import path from 'path';
-import createRequire from 'create-require';
 
 export type JestConfigOptions = Partial<Config.InitialOptions>;
 
@@ -8,32 +6,10 @@ export function createJestConfig(
   _: (relativePath: string) => void,
   rootDir: string
 ): JestConfigOptions {
-  function resolveRelativeTo(file: string, moduleSpecifier: string) {
-    const req = createRequire(file);
-    try {
-      return req.resolve(moduleSpecifier);
-    } catch {
-      return null;
-    }
-  }
-  function resolveBabelJest() {
-    const jestLocation =
-      resolveRelativeTo(path.join(rootDir, 'file.js'), 'jest') ||
-      require.resolve('jest');
-    const jestCoreLocation = resolveRelativeTo(jestLocation, '@jest/core')!;
-    const jestConfigLocation = resolveRelativeTo(
-      jestCoreLocation,
-      'jest-config'
-    )!;
-    return (
-      resolveRelativeTo(path.join(rootDir, 'file.js'), 'babel-jest') ||
-      resolveRelativeTo(jestConfigLocation, 'babel-jest')!
-    );
-  }
   const config: JestConfigOptions = {
     transform: {
       '.(ts|tsx)$': require.resolve('ts-jest/dist'),
-      '.(js|jsx)$': resolveBabelJest(), // jest's default
+      '.(js|jsx)$': require.resolve('babel-jest'), // jest's default
     },
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
